7233fd0483181c3d5fe46f1d42b8e16d1eb6e23f
Mariana13097 <marizaco@uninpahu.edu.co>
2025-04-23 21:32:08 -0500

    Add files via upload

---
+++ condicionales1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package marianacondicionalescondicionales;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class marianacondicionales {
+
+    public static void main(String[] args) {
+
+        String meses = "Octubre"; // Puedes cambiar a "Diciembre"
+
+        if (meses.equals("Octubre")) {
+            System.out.println("Pedir dulces");
+            System.out.println("Asustar");
+        } else if (meses.equals("Diciembre")) {
+            System.out.println("Navidad");
+            System.out.println("Regalos");
+            System.out.println("Buñuelos");
+            System.out.println("Papa Noel");
+        }
+    }
+}
